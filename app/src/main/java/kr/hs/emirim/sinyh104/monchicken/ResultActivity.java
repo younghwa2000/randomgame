c7aba1085c71de8da237902de0bf075a79fd5c4c
YoungHwa Shin <sinyh104@e-mail.hs.kr>
2016-03-28 20:18:58 +0900

    #2 add Result Activity

---
+++ app/src/main/java/kr/hs/emirim/sinyh104/monchicken/ResultActivity.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.sinyh104.monchicken;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by appcreator06 on 2016. 3. 28..
+ */
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
