c4fcf266608f4e0f7c1ea8debbe5c2b23242a3c2
YoungHwa Shin <sinyh104@e-mail.hs.kr>
2016-03-31 20:59:23 +0900

    #4 add button &onClickListener

--- app/src/main/java/kr/hs/emirim/sinyh104/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sinyh104/monchicken/MainActivity.java
@@ -2,12 +2,15 @@ package kr.hs.emirim.sinyh104.monchicken;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     EditText mName;
-
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +18,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mName = (EditText)findViewById(R.id.name);
+        mButton=(Button)findViewById(R.id.btn_show_me_the_chicks);
+        mButton.setOnClickListener(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         mName.setText("");
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this,"배고파요!",Toast.LENGTH_LONG).show();
     }
 }
