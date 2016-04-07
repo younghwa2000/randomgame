4186c67a863e0d33345ee19c866ebd73f5a31de8
YoungHwa Shin <sinyh104@e-mail.hs.kr>
2016-04-07 21:06:47 +0900

    #5 Show image

--- app/src/main/java/kr/hs/emirim/sinyh104/monchicken/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/sinyh104/monchicken/ResultActivity.java
@@ -3,15 +3,19 @@ package kr.hs.emirim.sinyh104.monchicken;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 /**
  * Created by appcreator06 on 2016. 3. 28..
  */
 public class ResultActivity extends AppCompatActivity {
-
+    static String TAG="먼치킨:ResultActivity";
     TextView mResult;
-
+    ImageView mImageView;
 
 
     @Override
@@ -19,14 +23,26 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
+        Log.d(TAG, "결과 액티비티 시작!");
         mResult=(TextView)findViewById(R.id.result);
+        mImageView=(ImageView) findViewById(R.id.imageView);
+        Log.d(TAG, "텍스트뷰 연결 성공!");
 
 
+        Random r=new Random();
+        int result=r.nextInt(4);
+
+        Log.d(TAG, "랜덤값 생성! : "+result);
+        mImageView.setImageResource(R.drawable.c01 + result);//중요
+        Log.d(TAG, "이미지리소스 값! : " + R.drawable.c01 + result);
 
         Intent intent=getIntent();
         String name=intent.getStringExtra("name");
-        int age=intent.getIntExtra("age",-1);
+        Log.d(TAG, "인텐트 값 읽기<name> : "+name);
+
+        int age=intent.getIntExtra("age", -1);
 
         mResult.setText(name+"님, 안녕하세요!!");
+        Log.d(TAG, "인텐트값 읽기<age> : "+ age);
     }
 }
