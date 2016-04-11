5ca6ba959b69df9a51855abbfdabe4bdb56e55dd
YoungHwa Shin <sinyh104@e-mail.hs.kr>
2016-04-11 21:13:18 +0900

    #8 SplashActivity

---
+++ app/src/main/java/kr/hs/emirim/sinyh104/monchicken/SplashActivity.java
@@ -0,0 +1,31 @@
+package kr.hs.emirim.sinyh104.monchicken;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();//현재 엑티비티 소멸
+            }
+        },3000);
+    }
+
+}
